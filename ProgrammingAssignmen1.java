f40a0f0fac76f6b0dfc656d86ade7d9e0b77e35c
Ricardo Dorancy <rdorancy@hotmail.com>
2018-03-14 23:35:22 -0400

    Update ProgrammingAssignmen1.java

--- ProgrammingAssignmen1.java
+++ ProgrammingAssignmen1.java
@@ -1,9 +1,6 @@
 /*
 Ricardo Dorancy 
-Professor Richmond 
-CIT 285
-Programming assignment 1 
-10/15/2016
+
 
 This program is developed for boola boola university. It's task is to get all students information
 and save them into mysql database. And another tasks is to registers students for different classes,
@@ -18,23 +15,6 @@ set up the quit option. Create a tableview  to under registration full time, par
 Creat tablbeview for for receivable and schedule option.Insert all the data into to dislay in the table, then 
 get a connect to mysql database. Create the different table in my sequel , and get a connection then insert the 
 data.
-
--Here is some students that I have saved in the database already
-
-'123456781', 'Mack', 'MD', 'Durand', '28 Otis st', 'Revere', 'CA', '2178', '10/05/2016', '2014', 'Senior', 'Associate Of Arts In Humanities', 'High School Diploma', 'Immunization'
-'123456782', 'James', 'JA', 'Abraham', '87 mass ave', 'Cambrigde', 'MA', '2344', '10/2/2016', '2013', 'Freshman', 'Associate Of Science In Computer Programming', 'High School Diploma', 'Immunization'
-'123456783', 'Jeff', 'JP', 'Paul', '24 Perl st', 'Smurl', 'CT', '2377', '10/2/2016', '2015', 'Sophomore', 'Associate Of Science In Computer Programming', 'High School Diploma', 'Immunization'
-'123456784', 'Ryck', 'RJ', 'Jhon', '23 Park st', 'Malden', 'CO', '2177', '12/17/2016', '2015', 'Sophomore', 'Associate Of Science In Computer Programming', 'High School Diploma', 'Immunization'
-'123456789', 'Bird', 'BM', 'Man', '23 Oak st', 'Malden', 'MA', '2134', '10/12/2016', '2015', 'Freshman', 'Associate Of Science In Computer Programming', 'High School Diploma', 'Immunization'
-'123456798', 'Jack', 'JR', 'Rick', '23 Bird st', 'Boston', 'MA', '2134', '10/12/2016', '2015', 'Freshman', 'Associate Of Science In Computer Programming', 'High School Diploma', 'Immunization'
-'222222229', 'Young', 'YJ', 'Jeezy', '24 Park st', 'Melbourne', 'AK', '2123', '10/12/2016', '2015', 'Junior', 'Associate Of Arts In Humanities', 'High School Diploma', 'Immunization'
-
--Here are some students that already register for clases
-'123456781', '123456781', 'Mack', 'MD', 'Durand', 'CMP237', 'C++ Programming', '3', '$300'
-'123456782', '123456782', 'James', 'JA', 'Abraham', 'OIM220', 'Keyboarding 1', '3', '$300'
-'123456789', '123456789', 'Bird', 'BM', 'Man', 'ENG111', 'English 1', '3', '$285'
-'123456798', '123456798', 'Jack', 'JR', 'Rich', 'ENG111', 'English 1', '3', '$285'
-'222222229', '222222229', 'Young', 'YJ', 'Jeezy', 'CMP545', 'Web Programming', '3', '$300'
 
 
  */
